3efd64757f043ad2bce858a5e9f058205dac8cc2
Jose Ramirez <jsayram@Gmail.com>
2018-01-14 21:40:52 -0500

    Add test for POST /users/login

--- server/server.js
+++ server/server.js
@@ -144,7 +144,6 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
@@ -157,9 +156,6 @@ app.post('/users/login', (req, res) => {
     });
 
 });
-
-
-
 
 
 
