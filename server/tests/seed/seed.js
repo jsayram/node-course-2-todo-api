c0438384269aae17b2b976e849ea384483c91d97
Jose Ramirez <jsayram@Gmail.com>
2018-01-14 23:12:26 -0500

    Update config to use json file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -17,7 +17,7 @@ const users = [{
 	password: 'userOnePass',
 	tokens:[{
 		access: 'auth',
-		token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+		token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
 	}]
 },{
 	_id: userTwoId,
@@ -25,7 +25,7 @@ const users = [{
 	password: 'userTwoPass',
 	tokens:[{
 		access: 'auth',
-		token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+		token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
 	}]
 }];
 
