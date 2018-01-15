a65f911a12e2feaac0494be29bbc232c8e8ca8ba
Jose Ramirez <jsayram@Gmail.com>
2018-01-14 21:55:30 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -59,6 +59,22 @@ UserSchema.methods.generateAuthToken = function() {
 };
 
 
+
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+    
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    });
+
+};
+
+
+
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
@@ -79,26 +95,26 @@ UserSchema.statics.findByToken = function(token) {
 };
 
 
-UserSchema.statics.findByCredentials = function (email,password) {
-	var User = this;
+UserSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
 
-	return User.findOne({email}).then((user)=>{
-		if(!user){
-			return Promise.reject();
-		}
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
 
-		return new Promise((resolve, reject)=>{
-			bcrypt.compare(password, user.password, (err, res)=>{
-				if(res){
-					resolve(user);
-				}else{
-					reject();
-				}
-			});
-		});
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
 
 
-	});
+    });
 
 };
 
@@ -113,8 +129,8 @@ UserSchema.pre('save', function(next) {
     if (user.isModified('password')) {
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
-            	user.password = hash;
-            	next();
+                user.password = hash;
+                next();
             });
         });
     } else {
